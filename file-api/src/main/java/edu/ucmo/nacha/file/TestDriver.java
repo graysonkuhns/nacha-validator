3959df5f25ca4f4ed869cea448e386df815c55ff
Grayson Kuhns <grayson.kuhns@xellitix.com>
2020-02-15 10:28:20 -0600

    Create DefaultEntryDetailParserTest

--- file-api/src/main/java/edu/ucmo/nacha/file/TestDriver.java
+++ file-api/src/main/java/edu/ucmo/nacha/file/TestDriver.java
@@ -15,28 +15,18 @@ public class TestDriver {
         new EntryDetailModule());
 
     EntryDetailParser parser = injector.getInstance(EntryDetailParser.class);
+
     EntryDetail entry = parser.parse(
-        "622101000019111111           00000000010200        " +
-            "   JONES DESMOND           0101000010000001");
-    System.out.println(entry.toString());
-
-    entry = parser.parse(
-        "6221010000199876543219875455500000000022660       " +
-            "    BOYD PATTIE             0101000010000016");
-    System.out.println(entry.toString());
-
-    entry = parser.parse(
-        "622101000019888999111000999  00000000092450" +
-            "           SHEARS WILLIAM          0101000010000009");
-    System.out.println(entry.toString());
-
-    entry = parser.parse(
-        "6321010000191234567890111213100000000021300   " +
+        "632101000019123456789011121310000000002       " +
             "        MUSTARD MISTER M        0101000010000002");
     System.out.println(entry.toString());
 
     entry = parser.parse(
-        "6321010000191234567890111213100000000021300   " +
+        "6321010000191234567890111213100000000021300           MUSTARD MISTER M        0101000010000002");
+    System.out.println(entry.toString());
+
+    entry = parser.parse(
+        "632101000019123456789011121310000000002       " +
             "        MUSTARD MISTER M      DD0101000010000002");
     System.out.println(entry.toString());
   }
