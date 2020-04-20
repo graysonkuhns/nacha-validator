355b14e284856978ddf5fed5e52f7d61241d2398
Garrett Ewens <40219097+Werken@users.noreply.github.com>
2020-04-20 17:30:12 -0500

    Create Dropwizard API module - S20C2-23 (#20)

---
+++ validation-service/src/main/java/edu/ucmo/nacha/service/ValidationServiceConfiguration.java
@@ -0,0 +1,10 @@
+package edu.ucmo.nacha.service;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class ValidationServiceConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
